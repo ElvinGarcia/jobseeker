fce28273dc7087329854750398c4fc6e09d28625
Elvin Garcia <dimix181@gmail.com>
2018-01-10 21:41:19 -0500

    added objects views and models

---
+++ app/models/applicant.rb
@@ -0,0 +1,10 @@
+class Applicamt < ActiveRecord::Base
+
+  include Slugifiable::Slug
+
+  def self.find_by_slug(slug)
+    normalize =  slug.gsub("-"," ").split.select{|x|x.capitalize!}.join(" ")
+    Artist.all.find_by({name: normalize})
+  end
+
+end
