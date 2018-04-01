81d5655664efc4db9951a0d699d53e7c76c5ec5a
Elvin Garcia <dimix181@gmail.com>
2018-04-01 20:47:53 +0900

    added more seed data to work with

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,22 @@
 
 
 ######
+Company.delete_all
+Applicant.delete_all
+Job.delete_all
 
-@spacex = Company.create(username:"spacex",name:"SpaceX", description:"Make humans multiplanetary", address:"Rocket Road Hawthorne, California", password:"000")
-
-
-@solar = Company.create(username:"solar",name:"Solar inc", description:"because it makes sense", address:"444 De Haro Street San Francisco, CA 94107", password:"000")
-
+@spacex = Company.create(username:"spacex",name:"SpaceX", description:"Make humans multiplanetary", address:"Rocket Road Hawthorne, California", password:"000000")
 
 @spacex.jobs.build(title:"spaceman", date:"17/03/2018",job_type:"fulltime",location:"Hawthorn CA", requirements:"none", salary:1000).save
 
-@spacex.jobs.build(title:"engineer", date:"17/03/2018",job_type:"fulltime",location:"Hawthorn CA", requirements:"none", salary:1000).save
+@solar = Company.create(username:"solar",name:"Solar inc", description:"because it makes sense", address:"444 De Haro Street San Francisco, CA 94107", password:"000000")
 
 @solar.jobs.build(title:"truck driver", date:"17/03/2018",job_type:"fulltime",location:"USA", requirements:"none", salary:1000).save
+
+@abby = Applicant.create(username:"abby", name:"Abby Cadabby", password:"123456")
+@elmo = Applicant.create(username:"elmo", name:"Elmo Red Monster", password:"123456")
+
+@elmo.jobs << @spacex.jobs.first
+@abby.jobs << @spacex.jobs.first
+@abby.jobs << @solar.jobs.first
+@elmo.jobs << @solar.jobs.first
